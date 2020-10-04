1094d0675e3c6246b7e8fa01bca4fdd50203ed2d
Asia Valentine <asiavalentine22@gmail.com>
2020-10-04 18:54:42 -0400

    header and home page

--- public/header.js
+++ public/header.js
@@ -7,34 +7,37 @@ var menuOpen = false;
 //function toggling the display of the menu and menu icon
 //adding or removing outside click function
 const openMenu = () => {
-    if (menuContent.style.display == 'flex') {
+    if (menuContent.style.display == 'block') {
       menuContent.style.display = 'none';
       closeBtn.style.display = 'none';
       hamburger.style.display = 'block';
       menuOpen = false;
     } else {
-      menuContent.style.display = 'flex';
+      menuContent.style.display = 'block';
       closeBtn.style.display = 'block';
       hamburger.style.display = 'none';
       menuOpen = true;
       
-      /*if(menuOpen == true) {
-        window.addEventListener('click', outsideClick);
+      if(menuOpen == true) {
+        //window.addEventListener('click', outsideClick);
+        console.log('menuopen is true');
       } else {
-        window.removeEventListener('click', outsideClick)
+        //window.removeEventListener('click', outsideClick)
+        console.log('menuopen is false');
       }
-    }*/
+    }
 
 } 
 
-/*
+
 function outsideClick(e) {
   var menuContent = document.getElementById('dropdown-menu-content');
   var menuBlock = document.getElementById('dropdown-menu-content').children[0];
+  
   if (e.target !== menuBlock && e.target !== hamburger) {
     menuContent.style.display = 'none';
     closeBtn.style.display = 'none';
     hamburger.style.display = 'block';
     menuOpen = false;
-  }*/
+  }
 }
